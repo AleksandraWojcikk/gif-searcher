ef4d0318bdd6c776ab434e6fd7c7340ee6b56d53
AleksandraWojcikk <aleksandra.wojcikk88@gmail.com>
2023-10-31 20:01:04 +0100

    gif-searcher

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,45 @@ import { useState } from 'react'
 
 import './App.css'
 
+const API_KEY = 'I4r912BEv1WazF6G9lpv9Q1NiNORASVC';
+const SEARCH_URL = 'https://api.giphy.com/v1/gifs/search?type=gifs&limit=10&api_key='
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [gifUrl, setGifUrl] = useState(null);
+
+  async function fetchData (query: string) {
+const response = await fetch(`${SEARCH_URL}${API_KEY}&q=${query}`);
+const {data} = await response.json();
+const randomNumber = Math.floor(Math.random()*9);
+const url = data[randomNumber].images.downsized_large.url;
+setGifUrl(url);
+console.log(data[0].images.downsized_large.url);
+
+  }
+
+  // zatwierdzanie inputu przez Enter
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    console.log(event.target);
+    const formData = new FormData(event.currentTarget);
+    const searchQuery = formData.get('gif-search');
+    console.log(searchQuery);
+
+    if(searchQuery) {
+    fetchData(searchQuery?.toString());}
+  }
 
   return (
     <>
-      <div>gif searcher
-       </div>
+      <h1>gif searcher
+       </h1>
+       <form onSubmit={handleSubmit}>
+<input type="search" name='gif-search' />
+<button type="reset">Clear</button>
+
+
+       </form>
+       {gifUrl && <img src={gifUrl} />}
     </>
   )
 }
